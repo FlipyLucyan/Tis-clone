c0df8782fb67b804541b9f828cc355ba8446b073
Lucius <lucy885@proton.me>
2025-08-21 22:34:00 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { LadingPage } from './components/lading-page/lading-page';
+import { NavBar } from './components/nav-bar/nav-bar';
+import { Footer } from './components/footer/footer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: LadingPage}
+];
+
+ 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from "./components/nav-bar/nav-bar";
+import { LadingPage } from "./components/lading-page/lading-page";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBar, LadingPage, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
